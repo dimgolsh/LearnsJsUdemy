eec027aa7dc03c51c55665a2882575e359902162
dimon <dimagol64@gmail.com>
2020-03-10 12:51:52 +0300

    add hw2

--- hw_2/script.js
+++ hw_2/script.js
@@ -55,6 +55,33 @@ while (i < 2) {
   }
 }
 */
+
+/*
+ let i = 0;
+
+do {
+  let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+  b = prompt("Во сколько обойдется?", "");
+
+  if (
+    typeof a === "string" &&
+    typeof a != null &&
+    typeof b != null &&
+    a != "" &&
+    b != "" &&
+    a.length < 50
+  ) {
+    console.log("done");
+    appData.expenses[a] = b;
+    i++;
+  } else {
+    i = 0;
+  }
+
+} while (i<2);
+
+*/
+
 appData.moneyPerDay = appData.budget / 30;
 
 console.log(appData);
