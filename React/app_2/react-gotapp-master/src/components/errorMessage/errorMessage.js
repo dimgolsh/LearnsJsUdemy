d7320c60de42ce029128fbb25c336bdc21270679
dimon <dimagol64@gmail.com>
2020-04-05 12:50:18 +0300

    error message

---
+++ React/app_2/react-gotapp-master/src/components/errorMessage/errorMessage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ErrorMessage = () => {
+    return <span>Error messagw</span>
+}
+
+export default ErrorMessage;
---
+++ React/app_2/react-gotapp-master/src/components/spinner/spinner.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./spinner.css";
+
+const Spinner = () => {
+  return (
+    <div class="loadingio-spinner-ripple-q0akervgheo">
+      <div class="ldio-uh1fh689x4">
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
