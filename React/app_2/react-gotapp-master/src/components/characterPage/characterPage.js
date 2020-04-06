8176e9cd097c94d9b22355a5d9f24d57d0865814
dimon <dimagol64@gmail.com>
2020-04-06 22:47:30 +0300

    rowBlock

--- React/app_2/react-gotapp-master/src/components/characterPage/characterPage.js
+++ React/app_2/react-gotapp-master/src/components/characterPage/characterPage.js
@@ -4,19 +4,9 @@ import ItemList from '../itemList';
 import CharDetails from "../charDetails";
 import ErrorMessage from '../errorMessage';
 import GotService from '../../services/gotService';
+import RowBlock from '../rowBlock';
 
-const RowBlock = ({left,right}) => {
-    return (
-        <Row>
-        <Col md="6">
-       {left}
-        </Col>
-        <Col md="6">
-      {right}
-        </Col>
-      </Row>
-    )
-}
+
 
 export default class CharacterPage extends Component {
 
