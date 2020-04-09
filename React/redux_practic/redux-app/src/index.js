2371ceaa5342b74240aad61b0bbd617989abd2e4
dimon <dimagol64@gmail.com>
2020-04-09 13:27:10 +0300

    redux store

--- React/redux_practic/redux-app/src/index.js
+++ React/redux_practic/redux-app/src/index.js
@@ -7,16 +7,38 @@ const reducer = (state = 0, action) => {
       return state + 1;
       break;
 
+    case 'DEC':
+    return state -1;
+    break;
+
+    case 'RND':
+      return state + action.value
     default:
       return state;
   }
 };
 
+const inc = () => ({ type: 'INC'});
+const dec = () => ({ type: 'DEC'});
+const rnd = (value) => ({ type: 'RND',value});
 const store = createStore(reducer);
 
-store.subscribe(()=>{
-  console.log(store.getState());
+document.getElementById('inc').addEventListener('click',()=>{
+  store.dispatch(dec())
+});
+document.getElementById('rnd').addEventListener('click',()=>{
+  const value = Math.floor(Math.random()*10);
+  store.dispatch(rnd(value))
+});
+document.getElementById('dec').addEventListener('click',()=>{
+  store.dispatch(inc())
 })
+
+const update = () => {
+  document.getElementById('counter').textContent=store.getState();
+}
+
+store.subscribe(update)
 
 
 store.dispatch({type: 'INC'});
