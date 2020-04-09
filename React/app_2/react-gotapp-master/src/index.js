3f65a69396d4411713ad797326084aba85082782
dimon <dimagol64@gmail.com>
2020-04-09 10:35:21 +0300

    hooks

--- React/app_2/react-gotapp-master/src/index.js
+++ React/app_2/react-gotapp-master/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import App from './components/app';
-import App from './components/app/appH';
+import App from './components/app';
+//import App from './components/app/appH';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
